80b7aea0d4073e78667caf620232800ceeea9e12
Logan Kloft <lkloft@outlook.com>
2024-12-01 17:47:31 -0500

    Complete Day 1

---
+++ day_1/part1.py
@@ -0,0 +1,18 @@
+left_nums = []
+right_nums = []
+
+with open("input.txt") as file:
+    for line in file:
+        left, right = line.strip().split()
+        left_nums.append(int(left))
+        right_nums.append(int(right))
+
+left_nums.sort()
+right_nums.sort()
+
+result = 0
+
+for i in range(len(left_nums)):
+    result = result + abs(left_nums[i] - right_nums[i])
+
+print(result)
---
+++ day_1/part2.py
@@ -0,0 +1,19 @@
+left_nums = []
+right_nums = []
+
+with open("input.txt") as file:
+    for line in file:
+        left, right = line.strip().split()
+        left_nums.append(int(left))
+        right_nums.append(int(right))
+
+right_freq = {}
+result = 0
+
+for num in right_nums:
+    right_freq[num] = right_freq.get(num, 0) + 1
+
+for i in range(len(left_nums)):
+    result = result + left_nums[i] * right_freq.get(left_nums[i], 0)
+
+print(result)
