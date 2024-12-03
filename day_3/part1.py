eeb47c41ca6ed6f2e0a494535932aa892150e143
Logan Kloft <lkloft@outlook.com>
2024-12-03 17:03:00 -0500

    Complete Day 3

---
+++ day_3/part1.py
@@ -0,0 +1,15 @@
+import re
+
+def get_x_y(mul_str):
+    x,y = re.findall(r"\d{1,3}", mul_str)
+    return int(x), int(y)
+
+result = 0
+with open("input.txt") as file:
+    for line in file:
+        matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", line.strip())
+        for match in matches:
+            x, y = get_x_y(match)
+            result += x * y
+
+print(result)
---
+++ day_3/part2.py
@@ -0,0 +1,21 @@
+import re
+
+def get_x_y(mul_str):
+    x,y = re.findall(r"\d{1,3}", mul_str)
+    return int(x), int(y)
+
+result = 0
+with open("input.txt") as file:
+    do = True
+    for line in file:
+        matches = re.findall(r"(don't\(\))|(do\(\))|(mul\(\d{1,3},\d{1,3}\))", line.strip())
+        for match in matches:
+            if match[0]:
+                do = False
+            elif match[1]:
+                do = True
+            elif do:
+                x, y = get_x_y(match[2])
+                result += x * y
+
+print(result)
